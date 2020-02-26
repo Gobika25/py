f7249309f7065183ab1de098aa035b9ba0109794
Gobika25 <52208659+Gobika25@users.noreply.github.com>
2020-02-26 09:43:27 +0530

    Create palindrome in given range.py

---
+++ palindrome in given range.py
@@ -0,0 +1,24 @@
+a=int(input())
+b=int(input())
+for i in range(a,b+1):
+    if i%10 == i//100:
+        print(i)
+    sir program
+    
+    
+    
+    
+    
+    
+    a=int(input())
+b=int(input())
+l=[]
+for i in range(a,b+1):
+    d=str(i)
+    if str(i) ==d[::-1] :
+        l.append(i)
+for j in l:
+    print(j,end=" ")
+    
+    
+    my program
