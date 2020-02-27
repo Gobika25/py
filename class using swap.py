751b7fbda927550a81a9dbadbaf9e8a45e61fd46
Gobika25 <52208659+Gobika25@users.noreply.github.com>
2020-02-27 10:53:28 +0530

    Update class using swap.py

--- class using swap.py
+++ class using swap.py
@@ -8,3 +8,17 @@ a,b=32,56
 p=Cse(a,b)
 Cse.swap(p)
 print(p.a,p.b)
+
+
+or  
+
+
+class Cse: #swap metod 2
+    def swap(self,a,b):
+        self.a=a
+        self.b=b
+        self.a,self.b=self.b,self.a
+a,b=32,56
+p=Cse()
+Cse.swap(p,a,b) #p.swap(a,b)
+print(p.a,p.b)
