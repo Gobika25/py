0518ed20f81d4c204e47e0d3245ede9b12c85508
Gobika25 <52208659+Gobika25@users.noreply.github.com>
2020-07-14 13:02:36 +0530

    Create plinland.py

---
+++ plinland.py
@@ -0,0 +1,25 @@
+Name: Gobika. T
+Date: 14-07-2020
+
+Program 3:
+
+a=int(input())
+for i in range(1,a+1):
+    b=int(input())
+    count = 0
+    while b>=1:
+        b=b//2
+        count=count+1
+    print(count)
+
+Explanation: The program logic is the input is get from the users in the given range. And the first input is 10 first we want to find the half of the given value so we want to divide the number by 2, like(b=10//2) so, we get 5 and this step is continue till the while condition should be false(b>=1) if b=0 then the while condition is false.
+Ip:
+2
+10
+5
+
+Op:
+4
+3
+
+
