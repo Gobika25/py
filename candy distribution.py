0c46f33342682769098fb0a01a3ae77739b3dd27
Gobika25 <52208659+Gobika25@users.noreply.github.com>
2020-07-14 13:03:58 +0530

    Create candy distribution.py

---
+++ candy distribution.py
@@ -0,0 +1,23 @@
+Name: Gobika.T
+Date: 14-7-2020
+
+Program 2:
+
+a=int(input())
+l=[]
+for i in range(a):
+    a,b,c=map(int,input().split(" "))
+    p=b%a
+    e=p+c-1
+    l.append(e)
+for i in l:
+    print(i)
+
+ip:
+2
+5 2 1
+5 2 2
+Op:
+2
+3
+
